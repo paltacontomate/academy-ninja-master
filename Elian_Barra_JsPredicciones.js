5f509fbcce9262fff226c96fdf6a410c82ffb189
paltacontomate <elian.barra.millaqueo@alumnos.sip.cl>
2022-05-10 10:15:12 -0400

    ninja academy
    Signed-off-by: paltacontomate <elian.barra.millaqueo@alumnos.sip.cl>

---
+++ Elian_Barra_JsPredicciones.js
@@ -0,0 +1,18 @@
+function myBirthYearFunc(){
+    console.log("Nací en " + 1980);
+}
+    // Console.log indica "naci en 1980 //
+
+    function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+    // console.log indica el año en que naci aplicando la funcion myBirthDay //
+
+    function add(num1, num2){
+        console.log("¡Sumando números!");
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+    // console.log esta mostrando la suma del num1 + el num2 //
---
+++ Elian_Barra_Predecir-Arreglos-y-condiciones_.js
@@ -0,0 +1,28 @@
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO",12345];
+    console.log(auntContactInfo);<Font></Font>
+}
+/*
+       // Variables               Datos //
+ auntContactInfo= ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO",12345];
+
+
+Fragmento de código 
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);<font></font>
+
+}         //Variables            Datos //
+        Produce        =    ["manzanas", "naranjas"];  
+							
+                
+Fragmento de código
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);<font></font>
+                //Variables  		           Datos  //  
+                Movie        =    ["Bambi", "E.T.", "Toy Story"];
+*/
